50eb403988c3ab27634d06ede3c6ce15c21c5464
shailesh04445 <100185744+shailesh04445@users.noreply.github.com>
2022-08-17 13:29:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// //difference between document & window object
+// ANS- Document represents any HTML document or web page that is loaded in browser , where window  represents
+// a browser  window or frame that displays the content of webpage . Document is loaded inside window and window
+// is a first object loaded in the browser .
+// We can access the document from the window using window.document and we can acess window in window only by
+// window.window . All the tags elements with attributres in html are the part of document where global objects
+// functions and variables pf javascript are numbers of window object .
+// The document is part of BOM and DOM  where window is only part of BOM
